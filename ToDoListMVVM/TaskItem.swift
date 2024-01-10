c257919af5a17d8281ef7fe7fca0ba4009212c23
AsetB <asetbakirov@gmail.com>
2024-01-10 15:10:58 +0600

    Lesson 14

---
+++ ToDoListMVVM/TaskItem.swift
@@ -0,0 +1,17 @@
+//
+//  TaskItem.swift
+//  ToDoListMVVM
+//
+//  Created by Aset Bakirov on 03.01.2024.
+//
+
+import Foundation
+
+struct TaskItem: Codable {
+    var name = ""
+    var isComplete = false
+    
+    init() {
+        
+    }
+}
--- ToDoListMVVM/ViewController.swift
+++ ToDoListMVVM/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    var viewModel: TaskViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewModel = TaskViewModel { [unowned self] (state) in
+            switch state.editingStyle {
+            case .addTask(let string):
+                textField.text = ""
+                break
+            case .deleteTask(let indexPath):
+                break
+            case .toggleTask(let indexPath):
+                break
+            case .loadTasks(let array):
+                break
+            case .none:
+                break
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        viewModel?.loadTasks()
+    }
+    
+    @IBAction func addTask(_ sender: Any) {
+        
+        viewModel?.addNewTask(taskName: textField.text!)
     }
 
 
